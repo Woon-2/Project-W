7394ac9d4d3be67efc3ddbb2559579df795564dd
Woon2 <wkdauddns9@gmail.com>
2024-08-16 12:08:01 +0900

    [refactor](Framework): replace identical variable names for variables in same function scope with different names

--- common/src/d3d12model.cpp
+++ common/src/d3d12model.cpp
@@ -14,9 +14,10 @@ Model::Model( d3d12::Core& core, d3d12::D3D12RenderContext& ctx, const gfx::Mode
         ), name );
     }
     for (const auto& child : model.children()) {
-        auto model = Model(core, ctx, child, vbUpIdx, ibUpIdx, pVbSerialIdx_, pIbSerialIdx_);
-        model.coord().setParent(&coordSys_);
-        children_.push_back(std::move(model));
+
+        auto mod = Model(core, ctx, child, vbUpIdx, ibUpIdx, pVbSerialIdx_, pIbSerialIdx_);
+        children_.push_back(std::move(mod));
+        children_.back().coord().setParent(&coordSys_);
     }
 }
 
