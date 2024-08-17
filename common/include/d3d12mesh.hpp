1bfabd2f853682eee146a9dfedd00a36d705f0f9
Woon2 <wkdauddns9@gmail.com>
2024-08-18 02:44:48 +0900

    [docs](Framework): write doxygen comments for gfx::d3d12::Mesh and gfx::d3d12::Model

--- common/include/d3d12mesh.hpp
+++ common/include/d3d12mesh.hpp
@@ -13,17 +13,74 @@ namespace gfx {
 
 namespace d3d12 {
 
+/**
+ * @brief A class representing a mesh in D3D12.     
+ * It contains a vertex buffer and an index buffer, and it can bind them to the graphics pipeline's input assembler stage.    
+ * Mesh::draw call count should be considered as draw call count, as unlike shader buffers, multiple input assembler settings cannot be bound at once.
+ * @details It creates D3D12 resources for the vertex buffer and the index buffer in the constructors,     
+ * and to upload the data to the GPU, auxiliary upload buffers are used.    
+ * Since the upload buffers is not more needed after the data is uploaded,    
+ * it is recommended to release them after the data is uploaded to the GPU.     
+ * The release of the upload buffers is done by calling Mesh::completeInit.    
+ * 
+ * @note The constructors adds graphics command list a command to copy the data from the upload buffers to the default buffers,    
+ * so you need to prepare the graphics command list to open and the data is uploaded to the GPU when the command list is executed.    
+ * In consequence, waiting for the command list to be executed is required before using the mesh or releasing the upload buffers.
+ * 
+ * sample code:    
+ * @code
+ * core.preRender();    // to open the graphics command list
+ * 
+ * auto mesh = gfx::d3d12::Mesh( core, ctx, gfx::loadMesh("path/to/mesh.obj"),
+ *     "myMesh1TmpUpBuf_vb", "myMesh1TmpUpBuf_ib"
+ * );
+ * 
+ * core.postRender();    // to close the graphics command list and execute it
+ * core.waitForGpu();    // to wait for the command list to be executed
+ * 
+ * mesh.completeInit(core);    // to release the upload buffers
+ * @endcode
+ * @note It is recommended to add Mesh object on DrawInfo object when rendering things with IScene implementation and IRenderer implementation,    
+ * as binding and drawing logic of meshes is a lot easier than directly using the d3d12 resources.    
+ * @see gfx::Mesh VertexBuffer Core Core::preRender Core::postRender Core::waitForGpu
+ */
 class Mesh {
 public:
     using IndexCont = gfx::Mesh::IndexCont;
 
+    /**
+     * @brief Constructs a d3d12 mesh with a vertex buffer and an index buffer.     
+     * It uploads the data to the GPU in same memory layout as the input buffers.
+     * @param core The D3D12 core object.
+     * @param ctx The D3D12 render context object.
+     * @param vbuf The vertex buffer.
+     * @param ibuf The index buffer.
+     * @param vbUpIdx The upload buffer index for the vertex buffer, which is used to register and to pop the upload buffer from the core.
+     * @param ibUpIdx The upload buffer index for the index buffer, which is used to register and to pop the upload buffer from the core.
+     * @see VertexBuffer Core Core::addTmpUpBuf Core::popTmpUpBuf Core::popTmpUpBufs
+     * @details It creates d3d12 resources internally and isn't in valid state until the gpu actually uploads the data.     
+     * Look at the class details to be acknowledged about proper construction of the mesh.
+     */
     Mesh( d3d12::Core& core, d3d12::D3D12RenderContext& ctx, const VertexBuffer& vbuf, const IndexCont& ibuf,
         Core::UpBufIdx vbUpIdx, Core::UpBufIdx ibUpIdx
     ) : vbView_(), ibView_(), vb_(), ib_(),
         vbUpIdx_(vbUpIdx), ibUpIdx_(ibUpIdx) {
         buildRes(core, ctx, vbuf, ibuf);
     }
-
+    /**
+     * @brief Constructs a d3d12 mesh with a vertex buffer and an index buffer.    
+     * It uploads the data to the GPU in same memory layout as the input buffers.
+     * @tparam R A range of 32-bit unsigned integers.
+     * @param core The D3D12 core object.
+     * @param ctx The D3D12 render context object.
+     * @param vb The vertex buffer.
+     * @param ib The index buffer.
+     * @param vbUpIdx The upload buffer index for the vertex buffer, which is used to register and to pop the upload buffer from the core.
+     * @param ibUpIdx The upload buffer index for the index buffer, which is used to register and to pop the upload buffer from the core.
+     * @details It creates d3d12 resources internally and isn't in valid state until the gpu actually uploads the data.     
+     * Look at the class details to be acknowledged about proper construction of the mesh.
+     * @see VertexBuffer Core Core::addTmpUpBuf Core::popTmpUpBuf Core::popTmpUpBufs
+     */
     template <std::ranges::range R>
     Mesh( d3d12::Core& core, d3d12::D3D12RenderContext& ctx, const VertexBuffer& vb, R&& ib,
         Core::UpBufIdx vbUpIdx, Core::UpBufIdx ibUpIdx
@@ -31,16 +88,46 @@ public:
         vbUpIdx_(vbUpIdx), ibUpIdx_(ibUpIdx) {
         buildRes(core, ctx, vb, IndexCont(std::begin(ib), std::end(ib)));
     }
-
+    /**
+     * @brief Constructs a d3d12 mesh with gfx::Mesh object.    
+     * It uploads the data to the GPU in same memory layout as the vertex buffer and the index buffer of the input mesh.
+     * @param core The D3D12 core object.
+     * @param ctx The D3D12 render context object.
+     * @param mesh The mesh object to copy the vertex buffer and the index buffer.
+     * @param vbUpIdx The upload buffer index for the vertex buffer, which is used to register and to pop the upload buffer from the core.
+     * @param ibUpIdx The upload buffer index for the index buffer, which is used to register and to pop the upload buffer from the core.
+     * @details It creates d3d12 resources internally and isn't in valid state until the gpu actually uploads the data.     
+     * Look at the class details to be acknowledged about proper construction of the mesh.
+     * @see VertexBuffer Core Core::addTmpUpBuf Core::popTmpUpBuf Core::popTmpUpBufs
+     */
     Mesh( d3d12::Core& core, d3d12::D3D12RenderContext& ctx, const gfx::Mesh& mesh,
         Core::UpBufIdx vbUpIdx, Core::UpBufIdx ibUpIdx
     ) : vbView_(), ibView_(), vb_(), ib_(),
         vbUpIdx_(vbUpIdx), ibUpIdx_(ibUpIdx) {
         buildRes(core, ctx, mesh.vb(), mesh.ib());
     }
-
+    /**
+     * @brief Pops the temporary upload buffers used to upload the data to the GPU from the core.
+     * @param core The D3D12 core object.
+     * @see Core Core::addTmpUpBuf Core::popTmpUpBuf Core::popTmpUpBufs
+     */
     void completeInit(d3d12::Core& core) const;
+
+    /**
+     * @brief Binds the mesh to the graphics pipeline's input assembler stage.
+     * @param pCmdList The graphics command list to bind the mesh.
+     * @details It binds the internal vertex buffer and the index buffer to the graphics pipeline's input assembler stage.
+     * @note It is recommended to add Mesh object on DrawInfo object when rendering things with IScene implementation and IRenderer implementation,    
+     * as binding and drawing logic of meshes is a lot easier than directly using the d3d12 resources.  
+     */
     void bind(ID3D12GraphicsCommandList* pCmdList) const;
+    /**
+     * @brief Draws the mesh.
+     * @param pCmdList The graphics command list to draw the mesh.
+     * @details The draw call is performed by calling `DrawIndexedInstanced` with the index buffer's index count.
+     * @note It is recommended to add Mesh object on DrawInfo object when rendering things with IScene implementation and IRenderer implementation,    
+     * as binding and drawing logic of meshes is a lot easier than directly using the d3d12 resources.  
+     */
     void draw(ID3D12GraphicsCommandList* pCmdList) const;
 
 private:
--- common/include/d3d12model.hpp
+++ common/include/d3d12model.hpp
@@ -16,6 +16,12 @@ namespace gfx {
 
 namespace d3d12 {
 
+/**
+ * @brief A class representing a model in D3D12.     
+ * Like gfx::Model, it structures a tree of meshes(scene graph), except that the contained meshes are Mesh, rather than gfx::Mesh.     
+ * For further information, see gfx::Model and Mesh.
+ * @see gfx::Model Mesh
+ */
 class Model {
 private:
     struct NamedMesh {
@@ -28,10 +34,41 @@ private:
     };
 
 public:
+    /**
+     * @brief keeping same structure as gfx::Model, it converts the gfx::Model's contained meshes to Mesh es.
+     * @param core The D3D12 core object.
+     * @param ctx The D3D12 render context object.
+     * @param model The gfx::Model to convert.
+     * @param vbUpIdx The upload buffer index for the vertex buffer, which is used to register and to pop the upload buffer from the core.
+     * @param ibUpIdx The upload buffer index for the index buffer, which is used to register and to pop the upload buffer from the core.
+     * @details As the Mesh isn't in valid state until the gpu actually uploads the data,     
+     * the Model also isn't in valid state until the gpu actually uploads the data.     
+     * 
+     * The temporary upload buffer indices for the vertex buffer and the index buffer is serialized and shared among the models in following way:    
+     * If the model is the root model to be created, the upload buffer index for it is suffixed with 0.    
+     * Then, the suffix number is incremented for each model visited while traversing the model tree,    
+     * reaching the number of (the count of models in the tree - 1).
+     * 
+     * for example, if the model tree is like below and `vbUpIdx` is "vb" and `ibUpIdx` is "ib":    
+     * root                : vb0, ib0    
+     * ├── child1          : vb1, ib1    
+     * │   ├── child1-1    : vb2, ib2     
+     * │   └── child1-2    : vb3, ib3    
+     * └── child2          : vb4, ib4    
+     * 
+     * the upload buffer indices for the root, child1, child1-1, child1-2, and child2 are    
+     * "vb0"&"ib0", "vb1"&"ib1", "vb2"&"ib2", "vb3"&"ib3", and "vb4"&"ib4" respectively.
+     * @see Mesh gfx::Model Core Core::addTmpUpBuf Core::popTmpUpBuf Core::popTmpUpBufs
+     */
     Model( d3d12::Core& core, d3d12::D3D12RenderContext& ctx, const gfx::Model& model,
         Core::UpBufIdx vbUpIdx, Core::UpBufIdx ibUpIdx
     ) : Model(core, ctx, model, vbUpIdx, ibUpIdx, std::make_shared<std::size_t>(0), std::make_shared<std::size_t>(0)) {}
 
+    /**
+     * @brief Calls completeInit for all the meshes in the model.
+     * @param core The D3D12 core object.
+     * @see Mesh::completeInit
+     */
     void completeInit(d3d12::Core& core) const;
 
     std::string_view name() const NOEXCEPT {
