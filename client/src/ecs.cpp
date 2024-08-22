8006b8a9eb65c500afc6bb65f537cae5163a4cf6
Woon2 <wkdauddns9@gmail.com>
2024-08-22 11:15:22 +0900

    [mod](Framework): erase expired entity in ecs's components and systems in ecs::DestroyEntity automatically.

--- client/src/ecs.cpp
+++ client/src/ecs.cpp
@@ -56,6 +56,9 @@ namespace ecs {
 			throw ECS_EXCEPT("Entity out of Range");
 		}
 
+		componentDestroyEntity(entity);
+		systemDestroyEntity(entity);
+
 		// std::bitset::reset
 		gSignature[entity].reset();
 
