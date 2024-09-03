95f6830baec469aa77ebb65bf357e8c53cdbb36e
Woon2 <wkdauddns9@gmail.com>
2024-09-04 05:07:28 +0900

    [feat][mod](Framework): add gfx::d3d12::UploadMemPool as a virtual memory pool for upload buffers, add resource barriers on gfx::d3d12::GpuMemPool's copy operation

--- common/include/gpuMem.hpp
+++ common/include/gpuMem.hpp
@@ -50,6 +50,33 @@ private:
     D3D12_GPU_VIRTUAL_ADDRESS head_;
 };
 
+class UploadMemPool {
+public:
+    UploadMemPool(Core& core, D3D12RenderContext& ctx, std::size_t blockSize, std::size_t blockCount);
+
+    D3D12_GPU_VIRTUAL_ADDRESS allocate();
+    void deallocate(D3D12_GPU_VIRTUAL_ADDRESS addr);
+    D3D12_GPU_VIRTUAL_ADDRESS construct_at( Core& core, D3D12RenderContext& ctx, D3D12_GPU_VIRTUAL_ADDRESS addr,
+        const void* pData, std::size_t bytes
+    );
+    template <std::ranges::contiguous_range R>
+        requires std::ranges::sized_range<R>
+    D3D12_GPU_VIRTUAL_ADDRESS construct_at( D3D12RenderContext& ctx, D3D12_GPU_VIRTUAL_ADDRESS addr,
+        const R& data
+    ) {
+        return construct_at(addr, std::data(data), std::size(data) * sizeof(std::ranges::range_value_t<R>));
+    }
+
+private:
+    void checkAddressValidity(D3D12_GPU_VIRTUAL_ADDRESS addr);
+
+    std::list<D3D12_GPU_VIRTUAL_ADDRESS> freeList_;
+    wrl::ComPtr<ID3D12Resource> buffer_;
+    std::size_t blockSize_;
+    std::size_t blockCount_;
+    D3D12_GPU_VIRTUAL_ADDRESS head_;
+};
+
 }   // namespace gfx::d3d12
 
 }   // namespace gfx
--- common/src/gpuMem.cpp
+++ common/src/gpuMem.cpp
@@ -1,5 +1,9 @@
 #include "gpuMem.hpp"
 
+#include "dxexcept.hpp"
+
+#include <cstdint>
+
 namespace gfx {
 
 namespace d3d12 {
@@ -60,9 +64,94 @@ void GpuMemPool::checkAddressValidity(D3D12_GPU_VIRTUAL_ADDRESS addr) {
 void GpuMemPool::copyAt( ID3D12GraphicsCommandList *pCmdList, D3D12_GPU_VIRTUAL_ADDRESS addr,
     ID3D12Resource *pSrcBuf, UINT64 offset, UINT64 bytes
 ) {
-    pCmdList->CopyBufferRegion(buffer_.Get(), addr - head_, pSrcBuf, offset, bytes);
+    // is this necessary?
+    auto bar = D3D12_RESOURCE_BARRIER{
+        .Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+        .Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE,
+        .Transition = {
+            .pResource = buffer_.Get(),
+            .Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+            .StateBefore = D3D12_RESOURCE_STATE_COMMON,
+            .StateAfter = D3D12_RESOURCE_STATE_COPY_SOURCE
+        }
+    };
+
+    DX_THROW_FAILED_VOID( pCmdList->CopyBufferRegion(buffer_.Get(), addr - head_, pSrcBuf, offset, bytes) );
+
+    // is this necessary?
+    bar = D3D12_RESOURCE_BARRIER{
+        .Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+        .Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE,
+        .Transition = {
+            .pResource = buffer_.Get(),
+            .Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+            .StateBefore = D3D12_RESOURCE_STATE_COPY_DEST,
+            .StateAfter = D3D12_RESOURCE_STATE_COMMON
+        }
+    };
+
+    DX_THROW_FAILED_VOID( pCmdList->ResourceBarrier(1, &bar) );
 }
 
-}  // namespace gfx::d3d12
+UploadMemPool::UploadMemPool( Core &core, D3D12RenderContext &ctx,
+    std::size_t blockSize, std::size_t blockCount
+) : freeList_(), buffer_( createUpBuf( core, static_cast<UINT64>(blockSize * blockCount) ) ),
+    blockSize_(blockSize), blockCount_(blockCount), head_(buffer_->GetGPUVirtualAddress()) {
+    auto freePtr = head_;
+
+    for (std::size_t i = 0; i < blockCount; ++i) {
+        freeList_.push_back(freePtr);
+        freePtr += blockSize;
+    }
+}
+
+D3D12_GPU_VIRTUAL_ADDRESS UploadMemPool::allocate() {
+    if (freeList_.empty()) {
+        return 0;
+    }
+
+    auto ret = freeList_.front();
+    freeList_.pop_front();
+    return ret;
+}
+
+void UploadMemPool::deallocate(D3D12_GPU_VIRTUAL_ADDRESS addr) {
+    checkAddressValidity(addr);
+    freeList_.push_back(addr);
+}
+
+D3D12_GPU_VIRTUAL_ADDRESS UploadMemPool::construct_at( Core& core, D3D12RenderContext& ctx,
+    D3D12_GPU_VIRTUAL_ADDRESS addr, const void* pData, std::size_t bytes
+) {
+    auto pCmdList = std::any_cast< wrl::ComPtr<ID3D12GraphicsCommandList> >(
+        ctx.cast(RenderContextType::D3D12)
+    );
+
+    checkAddressValidity(addr);
+
+    std::uint8_t* pMappedData = nullptr;
+
+    auto readRange = D3D12_RANGE{};
+    DX_THROW_FAILED( buffer_->Map(0, &readRange, reinterpret_cast<void**>(&pMappedData)) );
+
+    std::memcpy(pMappedData + (addr - head_), pData, bytes);
+
+    auto writeRange = D3D12_RANGE{ addr - head_, addr - head_ + bytes };
+    DX_THROW_FAILED_VOID( buffer_->Unmap(0, &writeRange) );
+
+    return addr;
+}
+
+void UploadMemPool::checkAddressValidity(D3D12_GPU_VIRTUAL_ADDRESS addr) {
+    if (addr < head_ || addr >= head_ + blockSize_ * blockCount_) {
+        throw std::runtime_error("Invalid address");
+    }
+
+    if ((addr - head_) % blockSize_ != 0) {
+        throw std::runtime_error("Invalid address");
+    }
+}
+
+} // namespace gfx::d3d12
 
 }   // namespace gfx
