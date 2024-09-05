7e7f4c499d2d244b2316b91dfbaacd5fb04e8b64
Woon2 <wkdauddns9@gmail.com>
2024-09-06 01:33:58 +0900

    [mod](Framework): populate tex2d buffer and texcube buffer in the same register space for share same root descriptor heap

--- client/shader/textures.hlsl
+++ client/shader/textures.hlsl
@@ -1,7 +1,6 @@
-// - TEX2D_SPACE
-// - TEXCUBE_SPACE
-// Above are the texture spaces used in the shader.
-// They are regarded as macros in the shader, and must be passed in as a define when compiling the shader.
+// - MAT_SPACE
+// Above is the texture space used in the shader.
+// It is regarded as macros in the shader, and must be passed in as a define when compiling the shader.
 
-Texture2D gTex2DLUT[] : register(t1, TEX2D_SPACE);
-TextureCube gTexCubeLUT[] : register(t1, TEXCUBE_SPACE);
+Texture2D gTex2DLUT[] : register(t1, MAT_SPACE);
+TextureCube gTexCubeLUT[] : register(t1, MAT_SPACE);
