0bb4bbf44213342ca9cbccdc8f7b9cef8ad05245
Woon2 <wkdauddns9@gmail.com>
2024-09-06 12:42:08 +0900

    [fix][feat](Framework): reflect configuration of shader with arguments from constructor, add textured phong shader

--- common/include/phongShader.hpp
+++ common/include/phongShader.hpp
@@ -14,45 +14,99 @@ namespace gfx {
 
 namespace d3d12 {
 
-	class PhongShaderNT : public Shader {
-    public:
-        static constexpr size_t defMaxInstances = 1000u;
-        static constexpr size_t defMaxMaterials = 24u;
-        static constexpr size_t defMaxLights = 12u;
+class PhongShaderNT : public Shader {
+public:
+    static constexpr size_t defMaxInstances = 1000u;
+    static constexpr size_t defMaxMaterials = 24u;
+    static constexpr size_t defMaxLights = 12u;
 
-        PhongShaderNT(Core& core, std::size_t duplicationCnt = 1u);
+    struct Config {
+        size_t maxInstances = defMaxInstances;
+        size_t maxMaterials = defMaxMaterials;
+        size_t maxLights = defMaxLights;
+    };
 
-        static constexpr std::string shaderName() NOEXCEPT {
-            return "PhongShaderNT";
-        }
+    PhongShaderNT(Core& core, const Config& config = Config{}, std::size_t duplicationCnt = 1u);
 
-        static Core::RootIdx rootName() NOEXCEPT {
-            return d3d12::rootName(RootPreset::Unified1);
-        }
+    static constexpr std::string shaderName() NOEXCEPT {
+        return "PhongShaderNT";
+    }
 
-        static Core::InputLayoutIdx inputLayoutName() NOEXCEPT {
-            return d3d12::inputLayoutName(InputLayoutPreset::Pos3Norm3Tex2);
-        }
+    static Core::RootIdx rootName() NOEXCEPT {
+        return d3d12::rootName(RootPreset::Unified);
+    }
 
-        GpuMappedRes& pfd() NOEXCEPT { return resPerFrameData_; }
-        GpuMappedRes& pdd() NOEXCEPT { return resPerDrawcallData_; }
-        GpuMappedRes& pid() NOEXCEPT { return resPerInstanceData_; }
-        GpuMappedRes& materials() NOEXCEPT { return resMaterials_; }
-        GpuMappedRes& lights() NOEXCEPT { return resLights_; }
+    static Core::InputLayoutIdx inputLayoutName() NOEXCEPT {
+        return d3d12::inputLayoutName(InputLayoutPreset::Pos3Norm3);
+    }
 
-    private:
-        std::vector< std::vector< wrl::ComPtr<ID3D12Resource> > > internalResArr_;
+    GpuMappedRes& pfd() NOEXCEPT { return resPerFrameData_; }
+    GpuMappedRes& pdd() NOEXCEPT { return resPerDrawcallData_; }
+    GpuMappedRes& pid() NOEXCEPT { return resPerInstanceData_; }
+    GpuMappedRes& materials() NOEXCEPT { return resMaterials_; }
+    GpuMappedRes& lights() NOEXCEPT { return resLights_; }
 
-        GpuMappedRes resPerFrameData_;
-        GpuMappedRes resPerDrawcallData_;
-        GpuMappedRes resPerInstanceData_;
-        GpuMappedRes resMaterials_;
-        GpuMappedRes resLights_;
+private:
+    std::vector< std::vector< wrl::ComPtr<ID3D12Resource> > > internalResArr_;
 
-        size_t maxInstances_;
-        size_t maxMaterials_;
-        size_t maxLights_;
-	};
+    GpuMappedRes resPerFrameData_;
+    GpuMappedRes resPerDrawcallData_;
+    GpuMappedRes resPerInstanceData_;
+    GpuMappedRes resMaterials_;
+    GpuMappedRes resLights_;
+
+    size_t maxInstances_;
+    size_t maxMaterials_;
+    size_t maxLights_;
+};
+
+class PhongShader : public Shader {
+public:
+    static constexpr std::size_t defMaxInstances = PhongShaderNT::defMaxInstances;
+    static constexpr std::size_t defMaxMaterials = PhongShaderNT::defMaxMaterials;
+    static constexpr std::size_t defMaxLights = PhongShaderNT::defMaxLights;
+
+    struct Config {
+        size_t maxInstances = defMaxInstances;
+        size_t maxMaterials = defMaxMaterials;
+        size_t maxLights = defMaxLights;
+    };
+
+    PhongShader(Core& core, const Config& config = Config{}, std::size_t duplicationCnt = 1u);
+
+    static constexpr std::string shaderName() NOEXCEPT {
+        return "PhongShader";
+    }
+
+    static Core::RootIdx rootName() NOEXCEPT {
+        return d3d12::rootName(RootPreset::Unified1);
+    }
+
+    static Core::InputLayoutIdx inputLayoutName() NOEXCEPT {
+        return d3d12::inputLayoutName(InputLayoutPreset::Pos3Norm3Tex2);
+    }
+
+    GpuMappedRes& pfd() NOEXCEPT { return resPerFrameData_; }
+    GpuMappedRes& pdd() NOEXCEPT { return resPerDrawcallData_; }
+    GpuMappedRes& pid() NOEXCEPT { return resPerInstanceData_; }
+    GpuMappedRes& materials() NOEXCEPT { return resMaterials_; }
+    GpuMappedRes& lights() NOEXCEPT { return resLights_; }
+    D3D12_GPU_DESCRIPTOR_HANDLE texSrvStart() NOEXCEPT { return texSrvStart_; }
+
+private:
+    std::vector< std::vector< wrl::ComPtr<ID3D12Resource> > > internalResArr_;
+
+    GpuMappedRes resPerFrameData_;
+    GpuMappedRes resPerDrawcallData_;
+    GpuMappedRes resPerInstanceData_;
+    GpuMappedRes resMaterials_;
+    GpuMappedRes resLights_;
+    D3D12_GPU_DESCRIPTOR_HANDLE texSrvStart_;
+
+    size_t maxInstances_;
+    size_t maxMaterials_;
+    size_t maxLights_;
+};
 
 
 }	// namespace gfx::d3d12
--- common/src/phongShader.cpp
+++ common/src/phongShader.cpp
@@ -2,14 +2,21 @@
 
 #include <ranges>
 
-gfx::d3d12::PhongShaderNT::PhongShaderNT(Core& core, std::size_t duplicationCnt) : Shader(),
+namespace gfx {
+
+namespace d3d12 {
+
+PhongShaderNT::PhongShaderNT(Core& core, const Config& config, std::size_t duplicationCnt)
+    : Shader(),
     internalResArr_(5, std::ranges::range_value_t<decltype(internalResArr_)>(duplicationCnt)),
     resPerFrameData_(core, sizeof(d3d12::sr::BasicPFD) , internalResArr_[0]),
     resPerDrawcallData_(core, sizeof(d3d12::sr::BasicPDD), internalResArr_[1]),
     resPerInstanceData_(core, sizeof(d3d12::sr::BasicPID), internalResArr_[2]),
-    resMaterials_(core, sizeof(d3d12::sr::PhongMaterial), internalResArr_[3]),
-    resLights_(core, sizeof(d3d12::sr::PhongLight), internalResArr_[4])
-{
+    resMaterials_(core, sizeof(d3d12::sr::PhongMaterialNT), internalResArr_[3]),
+    resLights_(core, sizeof(d3d12::sr::PhongLight), internalResArr_[4]),
+    maxInstances_(config.maxInstances),
+    maxMaterials_(config.maxMaterials),
+    maxLights_(config.maxLights) {
     auto builder = SimpleShaderBuilder();
     builder.code(Type::Vertex, loadCSO(compiledShaderPath / "ntShader_vs.cso"));
     builder.code(Type::Pixel, loadCSO(compiledShaderPath / "ntShader_ps.cso"));
@@ -22,3 +29,31 @@ gfx::d3d12::PhongShaderNT::PhongShaderNT(Core& core, std::size_t duplicationCnt)
     builder.setInputLayout(il).setRoot(pRoot).build(pDevice, *this, 0u);
     builder.wireframe().build(pDevice, *this, 1u);
 }
+
+PhongShader::PhongShader(Core &core, const Config &config, std::size_t duplicationCnt) 
+    : Shader(),
+    internalResArr_(5, std::ranges::range_value_t<decltype(internalResArr_)>(duplicationCnt)),
+    resPerFrameData_(core, sizeof(d3d12::sr::BasicPFD), internalResArr_[0]),
+    resPerDrawcallData_(core, sizeof(d3d12::sr::BasicPDD), internalResArr_[1]),
+    resPerInstanceData_(core, sizeof(d3d12::sr::BasicPID), internalResArr_[2]),
+    resMaterials_(core, sizeof(d3d12::sr::PhongMaterial), internalResArr_[3]),
+    resLights_(core, sizeof(d3d12::sr::PhongLight), internalResArr_[4]),
+    maxInstances_(config.maxInstances),
+    maxMaterials_(config.maxMaterials),
+    maxLights_(config.maxLights) {
+    auto builder = SimpleShaderBuilder();
+    builder.code(Type::Vertex, loadCSO(compiledShaderPath / "tShader_vs.cso"));
+    builder.code(Type::Pixel, loadCSO(compiledShaderPath / "tShader_ps.cso"));
+
+    auto pRoot = core.root(rootName());
+    auto pDevice = static_cast<ID3D12Device*>(DeviceFetcher::device(core));
+
+    const auto& il = core.inputLayout(inputLayoutName());
+
+    builder.setInputLayout(il).setRoot(pRoot).build(pDevice, *this, 0u);
+    builder.wireframe().build(pDevice, *this, 1u);
+}
+
+}   // namespace gfx::d3d12
+
+}   // namespace gfx
