2ea59e27756c7f0dd7ea628b9f508e0136d0ea0a
Woon2 <wkdauddns9@gmail.com>
2024-09-10 22:26:16 +0900

    [mod](PhongSample): make client sample game object's static resources public, add getter of world matrix for the game object

--- client/include/gun.hpp
+++ client/include/gun.hpp
@@ -2,18 +2,21 @@
 #define __GUN_HPP
 
 #include "coord.hpp"
+#include "drawInfo.hpp"
+
 #include "d3d12mesh.hpp"
 #include "d3d12material.hpp"
 
 class Gun {
 public:
-    static void loadAssets(gfx::d3d12::Core& core, std::size_t fenceIdx = 0u);
-    void MU_CALLCONV update(mu::Vec3 pos, mu::NQuat rot);
-
-private:
     static gfx::d3d12::Mesh sMesh;
     static gfx::d3d12::Material sMaterial;
 
+    static void loadAssets(gfx::d3d12::Core& core, std::size_t fenceIdx = 0u);
+    void MU_CALLCONV update(mu::Vec3 pos, mu::NQuat rot);
+    mu::Mat4x4 MU_CALLCONV world() const { return coord_.xform(); }
+
+private:
     gfx::coord::System coord_;
     gfx::d3d12::Mesh* mesh_;
     gfx::d3d12::Material* material_;
