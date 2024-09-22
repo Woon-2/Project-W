d218b83d7b0e385d8f0e2566fff046c30af7d830
Woon2 <wkdauddns9@gmail.com>
2024-09-22 15:08:36 +0900

    [feat](Framework): add client model

--- client/include/cmodel.hpp
+++ client/include/cmodel.hpp
@@ -1,4 +1,44 @@
 #ifndef __Client_Model_HPP
 #define __Client_Model_HPP
 
+#include "coord.hpp"
+
+#include "d3d12model.hpp"
+#include "d3d12materialTree.hpp"
+
+#include <vector>
+
+class Model {
+public:
+    Model(const gfx::d3d12::Model* srcModel, const gfx::d3d12::MaterialTree* srcMaterialTree);
+
+    void setCoordSys(const gfx::coord::System& coordSys) { coordSys_ = coordSys; }
+    gfx::coord::System& coord() NOEXCEPT { return coordSys_; }
+    const gfx::coord::System& coord() const NOEXCEPT { return coordSys_; }
+
+    const gfx::d3d12::Model* srcModel() const NOEXCEPT { return srcModel_; }
+    const gfx::d3d12::MaterialTree* srcMaterialTree() const NOEXCEPT { return srcMaterialTree_; }
+
+    const auto& children() const NOEXCEPT { return children_; }
+    auto& children() NOEXCEPT { return children_; }
+
+    void addChild(const Model& child) { children_.push_back(child); }
+    void addChild(Model&& child) { children_.push_back(std::move(child)); }
+    void emplaceChild( const gfx::d3d12::Model* srcModel,
+        const gfx::d3d12::MaterialTree* srcMaterialTree
+    ) {
+        children_.emplace_back(srcModel, srcMaterialTree);
+    }
+
+private:
+    static void syncHierarchy( const gfx::d3d12::Model& srcSubModel,
+        const gfx::d3d12::MaterialTree srcSubMaterialTree, Model& dstModel
+    );
+
+    gfx::coord::System coordSys_;
+    std::vector<Model> children_;
+    const gfx::d3d12::Model* srcModel_;
+    const gfx::d3d12::MaterialTree* srcMaterialTree_;
+};
+
 #endif // __Client_Model_HPP
--- client/src/cmodel.cpp
+++ client/src/cmodel.cpp
@@ -1,1 +1,23 @@
 #include "cmodel.hpp"
+
+Model::Model(const gfx::d3d12::Model* srcModel, const gfx::d3d12::MaterialTree* srcMaterialTree)
+    : coordSys_(srcModel->coord()), srcModel_(srcModel), srcMaterialTree_(srcMaterialTree) {
+    syncHierarchy(*srcModel, *srcMaterialTree, *this);
+}
+
+void Model::syncHierarchy( const gfx::d3d12::Model& srcSubModel,
+    const gfx::d3d12::MaterialTree srcSubMaterialTree, Model& dstModel
+) {
+    auto itModel = srcSubModel.children().begin();
+    auto itMaterial = srcSubMaterialTree.children().begin();
+
+    while (itModel != srcSubModel.children().end()) {
+        dstModel.emplaceChild(&*itModel, &*itMaterial);
+        dstModel.children().back().coord().setParent(&dstModel.coord());
+
+        syncHierarchy(*itModel, *itMaterial, dstModel.children().back());
+
+        ++itModel;
+        ++itMaterial;
+    }
+}
