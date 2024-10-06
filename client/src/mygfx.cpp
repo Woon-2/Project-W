5952401b7e4d505d8f32d85adf2fb92904222fc5
Woon2 <wkdauddns9@gmail.com>
2024-10-07 02:38:34 +0900

    [fix](Framework): postpone shader initialization to follow root signature and descriptor heap to be initialized as it uses them.

--- client/src/mygfx.cpp
+++ client/src/mygfx.cpp
@@ -7,16 +7,6 @@
 void MyGfx::init() {
     gfx::d3d12::Core::init();
 
-    phongShader_ = gfx::d3d12::PhongShader( *this,
-        gfx::d3d12::PhongShader::Config{ .maxInstCnt = 1000u, .maxLightCnt = 12u },
-        2u
-    );
-
-    phongShaderNT_ = gfx::d3d12::PhongShaderNT( *this,
-        gfx::d3d12::PhongShaderNT::Config{ .maxInstCnt = 1000u, .maxLightCnt = 12u },
-        2u
-    );
-
     addRoot( gfx::d3d12::rootName(gfx::d3d12::RootPreset::Unified),
         gfx::d3d12::makeRootPreset(*this, gfx::d3d12::RootPreset::Unified)
     );
@@ -30,9 +20,23 @@ void MyGfx::init() {
         gfx::d3d12::DescriptorHeap(pDevice, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 123u, true)
     );
 
+    phongShader_ = gfx::d3d12::PhongShader( *this,
+        gfx::d3d12::PhongShader::Config{ .maxInstCnt = 1000u, .maxLightCnt = 12u },
+        2u
+    );
+
+    phongShaderNT_ = gfx::d3d12::PhongShaderNT( *this,
+        gfx::d3d12::PhongShaderNT::Config{ .maxInstCnt = 1000u, .maxLightCnt = 12u },
+        2u
+    );
+
     illuminanceRenderer_.init(*this);
-    illuminanceRenderer_.pushShader( gfx::rp::Protocol::PhongInstancing, &phongShader_.value() );
-    illuminanceRenderer_.pushShader( gfx::rp::Protocol::PhongInstancingNT, &phongShaderNT_.value() );
+    illuminanceRenderer_.pushShader( gfx::rp::Protocol::PhongInstancing, &phongShader_.value(),
+        phongShader_.value().optSolidAndGeneral()
+    );
+    illuminanceRenderer_.pushShader( gfx::rp::Protocol::PhongInstancingNT, &phongShaderNT_.value(),
+        phongShaderNT_.value().optSolidAndGeneral()
+    );
 }
 
 void MyGfx::setFrame(std::size_t frameIdx) {
