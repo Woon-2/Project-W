b220f5c5a26b89afbd8837a717cd7356a7ea9239
Woon2 <wkdauddns9@gmail.com>
2024-10-07 00:47:44 +0900

    [mod](Framework): modify texture paths to use wic texture

--- client/src/assetMap.cpp
+++ client/src/assetMap.cpp
@@ -4,12 +4,12 @@
 namespace detail {
 const std::array<std::vector<AssetDesc>, 1> gAssetDescs = { { {
     AssetDesc{ AssetType::Model, "DragonModel", resourcePath/"models"/"dragon"/"Dragon 2.5_fbx.fbx" },
-    AssetDesc{ AssetType::Texture, "DragonTex", resourcePath/"models"/"dragon"/"textures"/"Dragon_ground_color.dds" },
-    AssetDesc{ AssetType::Texture, "DragonNormalMap", resourcePath/"models"/"dragon"/"textures"/"Dragon_Nor.dds" },
-    AssetDesc{ AssetType::Texture, "DragonBumpMap", resourcePath/"models"/"dragon"/"textures"/"Dragon_Bump_Col2.dds" },
-    AssetDesc{ AssetType::Texture, "FloorTex", resourcePath/"models"/"dragon"/"textures"/"Floor_C.dds"},
-    AssetDesc{ AssetType::Texture, "FloorNormalMap", resourcePath/"models"/"dragon"/"textures"/"Floor_N.dds"},
-    AssetDesc{ AssetType::Texture, "FloorSpecularMap", resourcePath/"models"/"dragon"/"textures"/"Floor_S.dds"},
+    AssetDesc{ AssetType::Texture, "DragonTex", resourcePath/"models"/"dragon"/"textures"/"Dragon_ground_color.jpg" },
+    AssetDesc{ AssetType::Texture, "DragonNormalMap", resourcePath/"models"/"dragon"/"textures"/"Dragon_Nor.jpg" },
+    AssetDesc{ AssetType::Texture, "DragonBumpMap", resourcePath/"models"/"dragon"/"textures"/"Dragon_Bump_Col2.jpg" },
+    AssetDesc{ AssetType::Texture, "FloorTex", resourcePath/"models"/"dragon"/"textures"/"Floor_C.jpg"},
+    AssetDesc{ AssetType::Texture, "FloorNormalMap", resourcePath/"models"/"dragon"/"textures"/"Floor_N.jpg"},
+    AssetDesc{ AssetType::Texture, "FloorSpecularMap", resourcePath/"models"/"dragon"/"textures"/"Floor_S.jpg"},
     AssetDesc{ AssetType::MaterialTree, "DragonMaterialTree", resourcePath/"models"/"dragon"/"Dragon 2.5_fbx.fbx" }
 } } };
 }   // namespace detail
