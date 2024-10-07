dc313ae1a2aa9983b392636dfc966973e84998c8
Woon2 <wkdauddns9@gmail.com>
2024-10-07 22:43:19 +0900

    [remove](Framework): remove legacy Client Model data

--- client/src/cmodel.cpp
+++ client/src/cmodel.cpp
@@ -35,28 +35,6 @@ void ModelDataXX::syncHierarchy( const gfx::d3d12::Model& srcSubModel,
     }
 }
 
-ModelData::ModelData(const gfx::d3d12::Model* srcModel, const gfx::d3d12::MaterialTree* srcMaterialTree)
-    : coordSys_(srcModel->coord()), srcModel_(srcModel), srcMaterialTree_(srcMaterialTree) {
-    syncHierarchy(*srcModel, *srcMaterialTree, *this);
-}
-
-void ModelData::syncHierarchy( const gfx::d3d12::Model& srcSubModel,
-    const gfx::d3d12::MaterialTree srcSubMaterialTree, ModelData& dstModel
-) {
-    auto itModel = srcSubModel.children().begin();
-    auto itMaterial = srcSubMaterialTree.children().begin();
-
-    while (itModel != srcSubModel.children().end()) {
-        dstModel.emplaceChild(&*itModel, &*itMaterial);
-        dstModel.children().back().coord().setParent(&dstModel.coord());
-
-        syncHierarchy(*itModel, *itMaterial, dstModel.children().back());
-
-        ++itModel;
-        ++itMaterial;
-    }
-}
-
 void Fragmentizer::addEntity(ecs::Entity& entity) {
     ecs::System<Model>::addEntity(entity);
     auto weakModel = entity.getC<Model>();
