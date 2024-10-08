c7c7545eeb0786490a739b9e53e32847c09edb64
Woon2 <wkdauddns9@gmail.com>
2024-10-09 04:38:57 +0900

    [fix](Framework): fix shader compile error

--- client/shader/shadowMapShader.hlsl
+++ client/shader/shadowMapShader.hlsl
@@ -6,13 +6,13 @@
 
 cbuffer PerFrameData : register(b1) {
     matrix gView2LightProj;
-}
+};
 
 struct VS_INPUT {
     float3 pos : POSITION;
     uint instID : SV_InstanceID;
-}
+};
 
 float4 VSMain(VS_INPUT input) : SV_POSITION {
-    return mul( mul( float4(pos, 1.f), getInstanceData(input.instID).wv ), gView2LightProj );
+    return mul( mul( float4(input.pos, 1.f), getInstanceData(input.instID).wv ), gView2LightProj );
 }
