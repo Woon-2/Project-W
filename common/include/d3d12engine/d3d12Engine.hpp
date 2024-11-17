6221feba898b84bfe3c1e7b125f5c24afd539a4d
Woon2 <wkdauddns9@gmail.com>
2024-11-17 14:53:59 +0900

    [feat](Framework): add core construction and rendering

--- common/include/d3d12engine/d3d12Engine.hpp
+++ common/include/d3d12engine/d3d12Engine.hpp
@@ -1,9 +1,50 @@
 #ifndef __D3D12Engine_HPP
 #define __D3D12Engine_HPP
 
+#include "d3d12engine/descriptorRangeSpec.hpp"
+
+#include "d3d12util/d3d12Low.hpp"
+#include "d3d12util/d3d12RenderPass.hpp"
+#include "d3d12util/d3d12ResourceXX.hpp"
+#include "d3d12util/d3d12ShaderXX.hpp"
+
 namespace gfx {
 
 namespace d3d12 {
+
+inline constexpr auto initialRtvHeapSize = 3u;
+inline constexpr auto initialDsvHeapSize = 1u;
+inline constexpr auto initialCbvSrvUavHeapSize = 1000u;
+
+class IRenderer {
+public:
+    virtual ~IRenderer() = default;
+    virtual void render(class Core&) = 0;
+};
+
+class Core {
+public:
+    using MyWindow = Window<BasicD3D12WTraits<char>>;
+
+    Core(dx::DXGIFactory& factory);
+
+    D3D12GfxCmdList fetchCmdList() {
+        return cmdList_;
+    }
+
+    void render(IRenderer& renderer);
+
+private:
+    D3D12Device device_;
+    D3D12CmdQueue cmdQueue_;
+    D3D12GfxCmdList cmdList_;
+    DescriptorHeapCPU rtvHeap_;
+    DescriptorHeapCPU dsvHeap_;
+    DescriptorHeapGPU cbvSrvUavHeap_;
+    DescriptorRanges descRanges_;
+    MyWindow window_;
+    Fence fence_;
+};
 
 }   // namespace gfx::d3d12
 
--- common/src/d3d12engine/d3d12Engine.cpp
+++ common/src/d3d12engine/d3d12Engine.cpp
@@ -1,1 +1,32 @@
 #include "d3d12engine/d3d12Engine.hpp"
+
+namespace gfx {
+
+namespace d3d12 {
+
+Core::Core(dx::DXGIFactory& factory)
+    : device_( getAvailableAdapter(factory, D3D_FEATURE_LEVEL_12_1), D3D_FEATURE_LEVEL_12_1 ),
+    cmdQueue_( device_ ), cmdList_( device_ ),
+    rtvHeap_(device_, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, initialRtvHeapSize),
+    dsvHeap_(device_, D3D12_DESCRIPTOR_HEAP_TYPE_DSV, initialDsvHeapSize),
+    cbvSrvUavHeap_(device_, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, initialCbvSrvUavHeapSize),
+    descRanges_(rtvHeap_, dsvHeap_, cbvSrvUavHeap_),
+    window_(), fence_(device_) {
+    window_.open( factory, device_, cmdQueue_,
+        descRanges_.rtvRangeBackBuf, descRanges_.dsvRangeBackBuf
+    );
+}
+
+void Core::render(IRenderer& renderer) {
+    cmdList_.reset();
+    renderer.render(*this);
+    cmdList_.close();
+    cmdQueue_.execute(cmdList_);
+    window_.present(cmdList_);
+    fence_.signal(cmdQueue_);
+    fence_.wait();
+}
+
+}   // namespace gfx::d3d12
+
+}   // namespace gfx
