fb7fbea8018b9cca31891acdb4fb69ea55e4986a
Lcat <ljj7403@naver.com>
2025-01-19 19:46:22 +0900

    [feat](Framework_Tessellation): Add Tessellation Render Pass class for ECS

--- common/include/d3d12engine/d3d12Engine.hpp
+++ common/include/d3d12engine/d3d12Engine.hpp
@@ -320,6 +320,14 @@ public:
     void update(Scene& scene) override;
 };
 
+class Tessellation : public IRenderPass, public d3d12::rp::Tessellation {
+public:
+    using d3d12::rp::Tessellation::Tessellation;
+
+    void init(Scene& scene) override;
+    void update(Scene& scene) override;
+};
+
 }   // namespace gfx::d3d12engine::rp
 
 }   // namespace gfx::d3d12engine
