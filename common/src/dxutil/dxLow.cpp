11c236a89054e347d258a891081366397f6ceb34
Woon2 <wkdauddns9@gmail.com>
2025-02-01 19:39:03 +0900

    [feat](Framework): embed directx math's some utiliites

--- common/src/dxutil/dxLow.cpp
+++ common/src/dxutil/dxLow.cpp
@@ -4,6 +4,20 @@ namespace gfx {
 
 namespace dx {
 
+DirectX::XMMATRIX XM_CALLCONV loadMat(const DirectX::XMFLOAT4X4& mat) {
+	return DirectX::XMLoadFloat4x4(&mat);
+}
+DirectX::XMMATRIX XM_CALLCONV loadMat(const DirectX::XMFLOAT3X3& mat) {
+	return DirectX::XMLoadFloat3x3(&mat);
+}
+
+void XM_CALLCONV storeMat(DirectX::FXMMATRIX src, DirectX::XMFLOAT4X4& dst) {
+	DirectX::XMStoreFloat4x4(&dst, src);
+}
+void XM_CALLCONV storeMat(DirectX::FXMMATRIX src, DirectX::XMFLOAT3X3& dst) {
+	DirectX::XMStoreFloat3x3(&dst, src);
+}
+
 DXGIFactory::DXGIFactory()
 	: dx::DXWrapper<IDXGIFactory4>() {
 #ifdef ENABLE_DXGI_INFO
