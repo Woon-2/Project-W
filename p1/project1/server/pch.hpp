5d12a744f822d78b6764990b8e8b81ba99f8c330
orange2458 <asm1133@naver.com>
2025-10-26 23:42:10 +0900

    헤더 파일과 편의기능 추가

---
+++ p1/project1/server/pch.hpp
@@ -0,0 +1,11 @@
+#ifndef SERVER_PCH_HPP
+#define SERVER_PCH_HPP
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+#include "macro.hpp"
+
+#endif // SERVER_PCH_HPP
