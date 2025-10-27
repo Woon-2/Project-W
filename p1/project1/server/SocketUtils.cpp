94365af1ec81f84fd58943dbb8783a63934e3984
orange2458 <asm1133@naver.com>
2025-10-27 20:30:56 +0900

    pch 적용

--- p1/project1/server/SocketUtils.cpp
+++ p1/project1/server/SocketUtils.cpp
@@ -1,3 +1,4 @@
+#include "pch.hpp"
 #include "NetAddress.hpp"
 #include "SocketUtils.hpp"
 
--- p1/project1/server/pch.hpp
+++ p1/project1/server/pch.hpp
@@ -1,6 +1,9 @@
 #ifndef SERVER_PCH_HPP
 #define SERVER_PCH_HPP
 
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <MSWSock.h>
