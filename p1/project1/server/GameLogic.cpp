a5d0a2a0c31a8503a221739bd8d89c04a97c00ef
orange2458 <asm1133@naver.com>
2025-11-24 23:57:42 +0900

    [server] GameLogic이 여러 Room들을 60fps로 update하는 거 구현 중
    메시지 처리는 계속하되, Room의 update는 60마다 처리함.
    메시지 처리에 시간이 걸린 경우, update를 여러 번 실행함.

--- p1/project1/server/GameLogic.cpp
+++ p1/project1/server/GameLogic.cpp
@@ -3,8 +3,42 @@
 
 void GameLogic::run() {
 	while (running_) {
-		for (auto& room : rooms_) {
-			room->update();
+		logicTimer_.tick();
+		accTime_ += logicTimer_.deltaTime<Milliseconds>().count();
+
+		while( auto msg = msgQueue_.try_dequeue_bulk()) {
+			// Process logic messages here
 		}
+
+		while (accTime_ >= logicUpdateInterval_) {
+			for (auto& room : rooms_) {
+				// Update each room's game logic here
+			}
+			accTime_ -= logicUpdateInterval_;
+		}
+
+		std::this_thread::yield();
+
+		//-------------------------------------------------------------------
+
+		//logicTimer_.tick();
+		//accTime_ += logicTimer_.deltaTime<Milliseconds>().count();
+
+		//if (accTime_ < logicUpdateInterval_) {
+		//	const auto sleepTime = logicUpdateInterval_ - accTime_;
+		//	std::this_thread::sleep_for(Milliseconds(sleepTime));
+		//}
+
+		//while (auto msg = msgQueue_.try_dequeue_bulk()) {
+		//	// Process logic messages here
+		//}
+
+		//for (auto& room : rooms_) {
+		//	// Update each room's game logic here
+		//}
+
+		//accTime_ = 0.f;
 	}
 }
+
+const float GameLogic::logicUpdateInterval_ = 16.6667f;
--- p1/project1/server/GameLogic.hpp
+++ p1/project1/server/GameLogic.hpp
@@ -1,9 +1,17 @@
 #ifndef GAME_LOGIC_HPP
 #define GAME_LOGIC_HPP
 
+#include "timer.hpp"
+
+struct LogicMessage {
+
+};
+
 class GameLogic {
 public:
-	GameLogic() : logicThread_(), running_(false), rooms_() {}
+	GameLogic() : logicThread_(), running_(false), msgQueue_(),
+		rooms_(), logicTimer_(), accTime_(0.f) {}
+
 
 	void start() {
 		running_ = true;
@@ -19,14 +27,17 @@ public:
 	}
 
 	void run();
-	
-	
 
 private:
 	std::thread logicThread_;
 	std::atomic_bool running_;
 
+	CCQueue<LogicMessage> msgQueue_;
 	std::vector<SPRoom> rooms_;
+
+	Timer logicTimer_;
+	float accTime_;
+	static const float logicUpdateInterval_;
 };
 
 #endif // GAME_LOGIC_HPP
